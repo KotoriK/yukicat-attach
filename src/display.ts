2bf7ceecb80a29591423c349cff7c4042c35a0ef
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-07-21 01:27:49 +0800

    src

--- src/display.ts
+++ src/display.ts
@@ -1,11 +1,16 @@
 import { importExternalFacePacks } from 'face-pack/src/FacePacksImporter'
 import FaceDisplay from 'face-pack/src/FaceDisplay/FaceDisplay'
-import { _attachListeners, showModal } from "simple-img-modal/src/deploy"
+import { _attachListeners, showModal, setImageModal, setContainer } from "simple-img-modal/src/deploy"
+import { ImageModalWithEXIF } from 'simple-img-modal/src/ImageModalWithEXIF'
 
 (async () => {
     const display = new FaceDisplay(await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json'))
-    _attachListeners(document.querySelectorAll('div.entry-content img:not(.avatar)'))
-    showModal()
+    const node = document.createElement('div')
+document.body.appendChild(node)
+setImageModal(ImageModalWithEXIF)
+setContainer(node)
+_attachListeners(document.querySelectorAll('div.entry-content img:not(.avatar)'))
+showModal()
     document.querySelectorAll('article.hentry p:not(.ct-respond-form-textarea):not(.form-submit)').forEach((v) => {
         display.render(v)
     })
--- src/imgmodal.ts
+++ src/imgmodal.ts
@@ -1,3 +1,8 @@
-import { _attachListeners, showModal } from "simple-img-modal/src/deploy"
+import { _attachListeners, showModal, setImageModal, setContainer } from "simple-img-modal/src/deploy"
+import { ImageModalWithEXIF } from 'simple-img-modal/src/ImageModalWithEXIF'
+export default function loadModal(){const node = document.createElement('div')
+document.body.appendChild(node)
+setImageModal(ImageModalWithEXIF)
+setContainer(node)
 _attachListeners(document.querySelectorAll('div.entry-content img:not(.avatar)'))
-showModal()
+showModal()}
