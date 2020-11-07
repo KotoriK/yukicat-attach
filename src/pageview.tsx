d91850043b64d0da6f4571a1dae293c7f8637d57
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-11-07 15:08:22 +0800

    Update pageview.tsx
    fix undefined

--- src/pageview.tsx
+++ src/pageview.tsx
@@ -1,5 +1,5 @@
 import useSWR from 'swr'
-import React, { CSSProperties, forwardRef, MutableRefObject, RefObject, useCallback, useState } from 'react'
+import React, { CSSProperties, forwardRef, useCallback, useState } from 'react'
 import { hydrate } from 'react-dom'
 import { usePopper } from 'react-popper'
 import 'simple-img-modal/src/FloatButton.css'
@@ -7,9 +7,9 @@ import 'simple-img-modal/src/SignedCollapse.css'
 const API_PREFIX = 'https://yukicat-ga-hit.vercel.app/api/ga/?page='
 const regNumber = /[0-9]{1,}/
 interface PVDate {
-    page: string,
+    page?: string,
     hit: string,
-    avgTOP: string
+    avgTOP?: string
 
 }
 function toTime(time: string | number) {
@@ -25,12 +25,13 @@ const DetailPannel = forwardRef<HTMLDivElement, { data: PVDate, style: CSSProper
             <li>
                 {`浏览量:${hit}`}
             </li>
-            <li>
-                {`平均浏览时间:${toTime(avgTOP)}`}
-            </li>
-            <li>
-                {`总浏览时间:${toTime(parseFloat(avgTOP) * parseInt(hit))}`}
-            </li>
+            {avgTOP ? <>
+                <li>
+                    {`平均浏览时间:${toTime(avgTOP)}`}
+                </li>
+                <li>
+                    {`总浏览时间:${toTime(parseFloat(avgTOP) * parseInt(hit))}`}
+                </li></> : null}
         </div>
 )
 const swrOption = {
@@ -39,15 +40,15 @@ const swrOption = {
 function PageView({ path: path_raw, raw }: { path: string, raw: string }) {
     const path = path_raw + (path_raw.endsWith('/') ? '' : '/')
     const { data, error } = useSWR<PVDate>(API_PREFIX + path, swrOption)
-    const _raw = raw.replace(/<\/?span>/g, '')
+    const _rawHit = raw.replace(/<\/?span>/g, '')
     const [refEle, setRefEle] = useState<HTMLSpanElement>()
     const [popperEle, setPopper] = useState<HTMLDivElement>()
     const [showPannel, setShowPannel] = useState(false)
     const { styles } = usePopper(refEle, popperEle, { placement: 'top', })
     const handleClick = useCallback(() => { setShowPannel(!showPannel) }, [showPannel, setShowPannel])
     return <>
-        <span onClick={handleClick} ref={setRefEle} data-raw={raw} className="clickable-sign">{error ? _raw.replace(regNumber, '-') : (data ? _raw.replace(regNumber, data[0].hit) : _raw)}</span>
-        <DetailPannel ref={setPopper} data={data} style={styles.popper} show={showPannel} />
+        <span onClick={handleClick} ref={setRefEle} data-raw={raw} className="clickable-sign">{error ? _rawHit.replace(regNumber, '-') : (data ? _rawHit.replace(regNumber, data[0].hit) : _rawHit)}</span>
+        <DetailPannel ref={setPopper} data={data || { hit: _rawHit }} style={styles.popper} show={showPannel} />
     </>
 }
 
