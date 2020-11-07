86dd6ebbef794dba3f0283b297b589f675f340a6
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-11-07 14:53:39 +0800

    update pv

--- src/pageview.tsx
+++ src/pageview.tsx
@@ -1,16 +1,56 @@
 import useSWR from 'swr'
-import React from 'react'
+import React, { CSSProperties, forwardRef, MutableRefObject, RefObject, useCallback, useState } from 'react'
 import { hydrate } from 'react-dom'
+import { usePopper } from 'react-popper'
+import 'simple-img-modal/src/FloatButton.css'
+import 'simple-img-modal/src/SignedCollapse.css'
 const API_PREFIX = 'https://yukicat-ga-hit.vercel.app/api/ga/?page='
-const reg = /[0-9]{1,}/
+const regNumber = /[0-9]{1,}/
+interface PVDate {
+    page: string,
+    hit: string,
+    avgTOP: string
+
+}
+function toTime(time: string | number) {
+    const _time = typeof time == 'string' ? parseFloat(time) : time//second
+    const minute = _time / 60
+    const second = (minute | 0 * 60)
+    const ms = second - (second | 0)
+    return `${minute > 0 && `${minute}分`}${second > 0 && `${ms > 0 ? second.toFixed(2) : second}秒`}`
+}
+const DetailPannel = forwardRef<HTMLDivElement, { data: PVDate, style: CSSProperties, show: boolean }>(
+    ({ data: { hit, avgTOP }, style, show }, ref) =>
+        <div className="popper opacity-trans" data-show={show} ref={ref} style={style}>
+            <li>
+                {`浏览量:${hit}`}
+            </li>
+            <li>
+                {`平均浏览时间:${toTime(avgTOP)}`}
+            </li>
+            <li>
+                {`总浏览时间:${toTime(parseFloat(avgTOP) * parseInt(hit))}`}
+            </li>
+        </div>
+)
+const swrOption = {
+    fetcher: (path) => fetch(path).then(r => r.json()), revalidateOnFocus: false, revalidateOnReconnect: false
+}
 function PageView({ path: path_raw, raw }: { path: string, raw: string }) {
     const path = path_raw + (path_raw.endsWith('/') ? '' : '/')
-    const { data, error } = useSWR(API_PREFIX + path, {
-        fetcher: (path) => fetch(path).then(r => r.json()), revalidateOnFocus: false, revalidateOnReconnect: false
-    })
-    const _raw = raw.replace(/<\/?span>/g,'')
-    return <span data-raw={raw}>{error ? _raw.replace(reg, '-') : (data ? _raw.replace(reg, data[0].hit) : _raw)}</span>
+    const { data, error } = useSWR<PVDate>(API_PREFIX + path, swrOption)
+    const _raw = raw.replace(/<\/?span>/g, '')
+    const [refEle, setRefEle] = useState<HTMLSpanElement>()
+    const [popperEle, setPopper] = useState<HTMLDivElement>()
+    const [showPannel, setShowPannel] = useState(false)
+    const { styles } = usePopper(refEle, popperEle, { placement: 'top', })
+    const handleClick = useCallback(() => { setShowPannel(!showPannel) }, [showPannel, setShowPannel])
+    return <>
+        <span onClick={handleClick} ref={setRefEle} data-raw={raw} className="clickable-sign">{error ? _raw.replace(regNumber, '-') : (data ? _raw.replace(regNumber, data[0].hit) : _raw)}</span>
+        <DetailPannel ref={setPopper} data={data} style={styles.popper} show={showPannel} />
+    </>
 }
+
 (() => {
     const colle = document.getElementsByClassName('meta-page-view')
     for (let i = 0; i < colle.length; i++) {
