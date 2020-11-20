33d1bad5dff1de9b939857712e2a3118eb2b3966
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-11-20 14:01:57 +0800

    update

--- src/fpselector.ts
+++ src/fpselector.ts
@@ -6,20 +6,20 @@ import FaceSelectorDeployer from 'face-pack/src/FaceSelector/FaceSelectorDeploye
 /*     const facePacks = DefaultFacePack
  */   new FaceSelectorDeployer({
         popcorn: document.getElementById('show-fs'),
-        tooltip: document.getElementById('fs-c'),
         facePackages: await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json'),
         onFaceSelected:
             (pack, face) => {
                 commentArea.value += `:${pack.id}.${face.id}:`
-            }, popperOptions: { placement: 'top' }, peakPopperOptions: {
-                placement: "right", modifiers: [
-                    {
-                        name: 'offset',
-                        options: {
-                            offset: [0, 20],
-                        },
+            },
+        popperOptions: { placement: 'top' }, peakPopperOptions: {
+            placement: "right", modifiers: [
+                {
+                    name: 'offset',
+                    options: {
+                        offset: [0, 20],
                     },
-                ],
-            }
+                },
+            ],
+        }
     }).render().hide()
 })()
