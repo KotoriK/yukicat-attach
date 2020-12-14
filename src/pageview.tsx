4c188fa490f9bbed28bc8e7999e492fed4cfc80c
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-12-14 21:55:51 +0800

    toTime：增加单位“小时”

--- src/pageview.tsx
+++ src/pageview.tsx
@@ -13,12 +13,14 @@ interface PVDate {
 
 }
 const { body: bodyRef } = document
-function toTime(time: string | number) {
-    const _min = (typeof time == 'string' ? parseFloat(time) : time) / 60//second
+function toTime(time: string | number,secFix:number = 2) {
+    const _hour = (typeof time == 'string' ? parseFloat(time) : time) / 3600//second
+    const hour = _hour | 0
+    const _min = (_hour - hour) * 60
     const min = _min | 0
     const second = (_min - min) * 60
     const ms = second - (second | 0)
-    return `${min > 0 ? `${min}分` : ''}${second > 0 ? `${ms > 0 ? second.toFixed(2) : second}秒` : ''}`
+    return `${hour > 0 ? `${hour}小时` : ''}${min > 0 ? `${min}分` : ''}${second > 0 ? `${ms > 0 ? second.toFixed(secFix) : second}秒` : ''}`
 }
 const DetailPannel = forwardRef<HTMLDivElement, { data: PVDate, style: CSSProperties, show: boolean }>(
     ({ data: { hit, avgTOP }, style, show }, ref) =>
@@ -31,7 +33,7 @@ const DetailPannel = forwardRef<HTMLDivElement, { data: PVDate, style: CSSProper
                     {`平均浏览时间:${toTime(avgTOP)}`}
                 </li>
                 <li>
-                    {`总浏览时间:${toTime(parseFloat(avgTOP) * parseInt(hit))}`}
+                    {`总浏览时间:${toTime(parseFloat(avgTOP) * parseInt(hit),0)}`}
                 </li></> : null}
         </div>
 )
