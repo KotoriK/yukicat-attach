93d2083257f6bbc3c0d775a9bdb947971cbab6e7
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-02 00:26:59 +0800

    使用emotion

--- src/pageview.tsx
+++ src/pageview.tsx
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import { CSSProperties, forwardRef, useEffect, useState } from 'react'
 import { createPortal, hydrate } from 'react-dom'
 import { usePopper } from 'react-popper'
-import { createUseStyles } from 'react-jss'
+import { css } from '@emotion/css'
 const API_PREFIX = 'https://yukicat-ga-hit.vercel.app/api/ga/?page='
 const regNumber = /[0-9]{1,}/
 interface PVDate {
@@ -12,35 +12,27 @@ interface PVDate {
 
 }
 const { body: bodyRef } = document
-const usePVStyles = createUseStyles(() => {
-    return {
-        clickable: {
-            cursor: "pointer"
-        }
-    }
+const styleClickable = css({
+    cursor: "pointer"
 })
-const usePannelStyle = createUseStyles(() => {
-    return {
-        popper: {
-            opacity: 0,
-            visibility: "hidden",
-            background: "#33333380",
-            color: "white",
-            padding: "4px 8px",
-            fontSize: "13px",
-            borderRadius: "4px",
-            maxHeight: "20vh",
-            overflow: "auto",
-            '&[data-show="true"]': {
-                opacity: 1,
-                visibility: "visible",
-                backdropFilter: "blur(3px)",
-            }, zIndex: 999
-        },
-        "opacity-trans": {
-            transition: "opacity 500ms ease-in-out",
-        },
-    }
+const popper = css({
+    opacity: 0,
+    visibility: "hidden",
+    background: "#33333380",
+    color: "white",
+    padding: "4px 8px",
+    fontSize: "13px",
+    borderRadius: "4px",
+    maxHeight: "20vh",
+    overflow: "auto",
+    '&[data-show="true"]': {
+        opacity: 1,
+        visibility: "visible",
+        backdropFilter: "blur(3px)",
+    }, zIndex: 999
+})
+const opacity_trans = css({
+    transition: "opacity 500ms ease-in-out",
 })
 const toTime = (time: string | number, secFix: number = 2) => {
     const _hour = (typeof time == 'string' ? parseFloat(time) : time) / 3600//second
@@ -53,8 +45,7 @@ const toTime = (time: string | number, secFix: number = 2) => {
 }
 const DetailPannel = forwardRef<HTMLDivElement, { data: PVDate, style: CSSProperties, show: boolean }>(
     ({ data: { hit, avgTOP }, style, show }, ref) => {
-        const styles = usePannelStyle()
-        return <div className={styles.popper + ' ' + styles['opacity-trans']} data-show={show} ref={ref} style={style}>
+        return <div className={popper + ' ' + opacity_trans} data-show={show} ref={ref} style={style}>
             <li>
                 {`浏览量:${hit}`}
             </li>
@@ -79,14 +70,13 @@ export function PageView({ path: path_raw, raw }: { path: string, raw: string })
     const [popperEle, setPopper] = useState<HTMLDivElement>()
     const [showPannel, setShowPannel] = useState(false)
     const [ticker, setTicker] = useState<number>()
-    const { styles, update } = usePopper(refEle, popperEle, { placement: 'top', modifiers: [{ name: 'eventListeners', options: { resize: true, scroll: showPannel } }] })
+    const { styles } = usePopper(refEle, popperEle, { placement: 'top', modifiers: [{ name: 'eventListeners', options: { resize: true, scroll: showPannel } }] })
     /* const setShowPannel = useCallback((value:boolean)=>{
         if(value){
             state.
         }
         _setShowPannel(value)
     },[_setShowPannel]) */
-    const pvStyles = usePVStyles()
     useEffect(() => () => clearTimeout(ticker), [])        //cleaner
     return <>
         <span onClick={() => {
@@ -96,7 +86,9 @@ export function PageView({ path: path_raw, raw }: { path: string, raw: string })
                 setShowPannel(showPannel)
                 setTicker(undefined)
             }, 5000))
-        }} ref={setRefEle} data-raw={raw} className={pvStyles.clickable}>{error ? _rawHit.replace(regNumber, '-') : (data ? _rawHit.replace(regNumber, data[0].hit) : _rawHit)}</span>
+        }} ref={setRefEle} data-raw={raw} className={styleClickable}>
+            {error ? _rawHit.replace(regNumber, '-') : (data ? _rawHit.replace(regNumber, data[0].hit) : _rawHit)}
+        </span>
         {createPortal(<DetailPannel ref={setPopper} data={(data && data[0]) || { hit: _rawHit }} style={styles.popper} show={showPannel} />
             , bodyRef)}
     </>
