91eb795213cb3c6c55b546fd119e8128d1d44cf7
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-03 13:25:02 +0800

    Delete imgmodal.ts

--- src/imgmodal.ts
+++
@@ -1,8 +0,0 @@
-import { attachListeners, showModal, setImageModal, setContainer } from "simple-img-modal/src/deploy"
-import { ImageModalWithEXIF } from 'simple-img-modal/src/ImageModalWithEXIF'
-const node = document.createElement('div')
-document.body.appendChild(node)
-setImageModal(ImageModalWithEXIF)
-setContainer(node)
-attachListeners(document.querySelectorAll('div.entry-content img:not(.avatar)'))
-showModal()
