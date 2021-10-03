39826f2983b7cc03e51cbf49b4bd8f3e23b9159a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-03 13:24:58 +0800

    fix: .emotion-box可能不存在

--- src/fpselector.ts
+++ src/fpselector.ts
@@ -3,24 +3,28 @@ import { FacePackage } from 'face-pack/src/FacePackage'
 import FaceSelectorDeployer from 'face-pack/src/selector/deployer/FaceSelectorDeployer'
 
 export default (facePackages: FacePackage[]) => {
-    const comment = document.querySelector('#comment')
-    const handleSelect = (pack, face) => (comment as HTMLTextAreaElement).value += `:${pack.id}.${face.id}:`
-    const deployer = new FaceSelectorDeployer({
-        facePackages,
-        onFaceSelected: handleSelect,
-        peakPopperOptions: {
-            placement: "right", modifiers: [
-                {
-                    name: 'offset',
-                    options: {
-                        offset: [0, 20],
+    const emotionBox = document.querySelector<HTMLElement>('.emotion-box')
+    if (emotionBox) {
+        const comment = document.querySelector('#comment')
+        const handleSelect = (pack, face) => (comment as HTMLTextAreaElement).value += `:${pack.id}.${face.id}:`
+        const deployer = new FaceSelectorDeployer({
+            facePackages,
+            onFaceSelected: handleSelect,
+            peakPopperOptions: {
+                placement: "right",
+                modifiers: [
+                    {
+                        name: 'offset',
+                        options: {
+                            offset: [0, 20],
+                        },
                     },
-                },
-            ],
-        }, self: document.querySelector('.emotion-box'),className:' '
+                ],
+            }, self: emotionBox, className: ' '
 
-    }).render().switchHide()
-    document.getElementById('emotion-toggle').addEventListener('click',()=>{
-        deployer.switchHide()
-    })
+        }).render().switchHide()
+        document.getElementById('emotion-toggle').addEventListener('click', () => {
+            deployer.switchHide()
+        })
+    }
 }
